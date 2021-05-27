89a13bb7a8f8a767cbbfc498fc4154a6ce289d45
rishiwhj <sharmarishii1983@gmail.com>
2021-05-27 13:06:10 +0530

    Rishi

--- sketch.js
+++ sketch.js
@@ -9,9 +9,11 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var bg="sprites/bg.png";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+backgroundImg=loadImage("sprites/bg.png");
+
 }
 
 function setup(){
@@ -45,8 +47,10 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
+    if (backgroundImg)
+        background(backgroundImg);
+    
+     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
@@ -66,7 +70,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display(); 
+    
+    getTime();
 }
 
 function mouseDragged(){
@@ -85,4 +91,23 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async  function   getTime(){
+var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+var responseJSON=await response.json();
+ //console.log(responseJSON);
+ var datetime=responseJSON.datetime;
+ //console.log(datetime);
+ var hour=datetime.slice(11,13);
+ console.log(hour);
+ if (hour>=06 && hour<=19){
+  bg="sprites/bg.png" ;
+
+ }
+ else {
+    bg="sprites/bg2.jpg";
+ }
+backgroundImg=loadImage(bg);
+
 }
